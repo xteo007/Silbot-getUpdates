631a56947d35dc36e86ec524b5598e586191a1c8
SilverOS <36743499+SilverOS@users.noreply.github.com>
2018-10-07 15:08:49 +0200

    🔰v1.1.1 Ho finalmente identato la base

--- db_install.php
+++ db_install.php
@@ -1,27 +1,26 @@
 <?php
 ins:
-$config['tipo_db'] =readLine("Tipo database (json/mysql):");
+$config['tipo_db'] = readLine("Tipo database (json/mysql):");
 if ($config['tipo_db'] == "json") {
-	touch("database.json");
-echo "DATABASE INSTALLATO";	
-	
+    touch("database.json");
+    echo "DATABASE INSTALLATO";
+    
 } elseif ($config['tipo_db'] == "mysql") {
-$config['ip'] =readLine("IP database (def. Localhost):");
-$config['user'] =readLine("User database (def. root):");
-$config['database'] =readLine("Nome database:");
-$config['password'] =readLine("Password database:");
-$config['tabella'] =readLine("Tabella database :");
-$dbh = new PDO("mysql:host=" . $config["ip"] . ";dbname=".$config['database'], $config['user'], $config['password']); 
-$tabella = $config['tabella'];
-$dbh->query("CREATE TABLE IF NOT EXISTS ".$tabella." (
+    $config['ip']       = readLine("IP database (def. Localhost):");
+    $config['user']     = readLine("User database (def. root):");
+    $config['database'] = readLine("Nome database:");
+    $config['password'] = readLine("Password database:");
+    $config['tabella']  = readLine("Tabella database :");
+    $dbh                = new PDO("mysql:host=" . $config["ip"] . ";dbname=" . $config['database'], $config['user'], $config['password']);
+    $tabella            = $config['tabella'];
+    $dbh->query("CREATE TABLE IF NOT EXISTS " . $tabella . " (
 id int(0) AUTO_INCREMENT,
 chat_id bigint(0),
 username varchar(200),
 page varchar(200),
 PRIMARY KEY (id))");
-echo "Database installato.";
+    echo "Database installato.";
 } else {
-echo "Errore: Tipo Database non trovato\n";
-goto ins;
+    echo "Errore: Tipo Database non trovato\n";
+    goto ins;
 }
-
